d904350526a2cc089f4fc5f8322bb1d8e5be3aa5
shapirodaniel <shapirodanieladam@gmail.com>
2022-02-23 12:46:46 -0600

    update: scope examples

--- scope/scope.js
+++ scope/scope.js
@@ -16,6 +16,22 @@ myFunction(7);
 // is only accessible inside the scope of myFunction
 /* console.log(number); */
 
+// execution context looks for things in its local scope first
+// then climbs the scope chain until it reaches the global scope
+// if the reference isn't found in the global scope, a ReferenceError will be thrown
+
+let magicNumber = 10;
+
+function printMagicNumber(magicNumber) {
+  console.log(magicNumber);
+}
+
+// logs 111
+printMagicNumber(111);
+
+// logs undefined -- why, if magicNumber is defined in outer scope ??
+printMagicNumber();
+
 // let and const are BLOCK scoped variables
 // this means that they're only "in-scope" inside curlies
 function myNestedScopes(number) {
